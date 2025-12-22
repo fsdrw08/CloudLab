f9610e0dbd976b132ca823de2acf460c61b0e3e8
fsdrw08 <drw_08@hotmail.com>
2025-12-22 17:13:18 +0800

    feat(storage): add Google Cloud Storage bucket configuration with lifecycle rules

---
+++ GCP/simple_bucket/main.tf
@@ -0,0 +1,43 @@
+resource "google_storage_bucket" "default" {
+  name          = "terraform-tfstate"
+  force_destroy = false
+  location      = "ASIA-EAST2"
+  storage_class = "STANDARD"
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age                        = 0
+      days_since_custom_time     = 0
+      days_since_noncurrent_time = 0
+      matches_prefix             = []
+      matches_storage_class      = []
+      matches_suffix             = []
+      num_newer_versions         = 1
+      with_state                 = "ARCHIVED"
+    }
+  }
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age                        = 0
+      days_since_custom_time     = 0
+      days_since_noncurrent_time = 1
+      matches_prefix             = []
+      matches_storage_class      = []
+      matches_suffix             = []
+      num_newer_versions         = 0
+      with_state                 = "ANY"
+    }
+  }
+
+  timeouts {}
+
+}
