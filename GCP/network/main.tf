881eb48bf212b92e71381cc36477b43c4594395f
fsdrw08 <drw_08@hotmail.com>
2025-12-23 17:50:28 +0800

    feat(network): add initial Google Compute Network and variables for project ID and VPC name

---
+++ GCP/network/main.tf
@@ -0,0 +1,7 @@
+resource "google_compute_network" "network" {
+  project                 = var.project_id
+  name                    = var.vpc_name
+  auto_create_subnetworks = true
+  mtu                     = 1460
+  bgp_best_path_selection_mode = "STANDARD"
+}
